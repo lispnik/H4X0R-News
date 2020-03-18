2463cbb5fe87178b79514cc4ef4d9f1af3819b5b
Matthew Kennedy <burnsidemk@gmail.com>
2020-03-17 21:20:00 -0500

    Add observer design pattern ObservedObject, ObservableObject

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,12 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("H4X0R News")
+        }
+        .onAppear() {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -24,14 +32,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Hola"),
-]
---
+++ Models/NetworkManager.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkManager.swift
+//  H4X0R News
+//
+//  Created by Matthew Kennedy on 3/17/20.
+//  Copyright © 2020 Matthew Kennedy. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
+
+    func fetchData() {
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let data = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: data)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
