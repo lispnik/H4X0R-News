6806a7095fa3c4b78cf82469c292b857399c0ad7
Matthew Kennedy <burnsidemk@gmail.com>
2020-03-17 20:27:52 -0500

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Matthew Kennedy on 3/17/20.
+//  Copyright © 2020 Matthew Kennedy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
